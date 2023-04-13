d9738c1242529bd111db000eda384eda1b2b2f83
cmbbinho <cmbbdu89@gmail.com>
2023-04-13 21:35:24 +0200

    new variables and updated classes

--- model.py
+++ model.py
@@ -13,11 +13,12 @@ class Icp(float):
     def to_eusd(self):
         return self.value*Icp.current_value
 class Protocol:
-    def __init__(self,current_collateral:Icp, issued_eusd:float=0,fee_available:float=0,base_fee:float=0):
+    def __init__(self,current_collateral:Icp, issued_eusd:float=0,fee_available:Icp=Icp(0),base_fee:float=0):
         self.current_collateral = current_collateral
         self.issued_eusd = issued_eusd
         self.fee_available  = fee_available
         self.base_fee=base_fee
+        self.supporters = {}
     @property
     def current_price(self):
         return Icp.current_value
@@ -39,6 +40,9 @@ class Protocol:
         if self.collateral_ratio>120:
             return 0
         return 2.5*(120-self.collateral_ratio)/40
+    def calculate_fee(self,input_amount:Icp)->Icp:
+        return Icp(input_amount*self.percentage_fee+self.base_fee) if isinstance(input_amount,Icp) else Icp(Icp(input_amount/Icp.current_value)*self.percentage_fee+self.base_fee)
+
     def burnt(self,amount_eusd):
         if amount_eusd<=self.issued_eusd:
             self.issued_eusd-=amount_eusd
@@ -47,16 +51,13 @@ class Protocol:
 
 
 class Levrager:
-    def __init__(self,protocol:Protocol,levrager_assets:Icp,stability_assets:Icp,initial_price:float):
-        self.levrager_assets = levrager_assets
+    def __init__(self,protocol:Protocol,levrager_assets:Icp,stability_assets:Icp):
         self.protocol = protocol
+        self.levrager_assets = levrager_assets
         self.stability_assets = stability_assets
-        self.initial_price = initial_price
-        self.protocol.fee_available+=levrager_assets*self.protocol.percentage_fee+self.protocol.base_fee
-    
-    @property
-    def liquidation_price(self):
-        return self.initial_price*self.stability_assets/(self.stability_assets+self.levrager_assets)
+        self.initial_price = Icp.current_value
+        self.protocol.fee_available+= self.protocol(levrager_assets)
+        self.liquidation_price = self.initial_price*self.stability_assets/(self.stability_assets+self.levrager_assets)
 
     @property
     def roi(self):
@@ -75,40 +76,61 @@ class Levrager:
     
 
 class CollateralSupporter:
-    def __init__(self,protocol:Protocol,elp_stacked,elp_yield,dissolve_delay,age_bonus,user_collateral:Icp):
+    def __init__(self,protocol:Protocol,elp_stacked,elp_yield,dissolve_delay,age_bonus):
+        self.protocol = protocol
         self.elp_stacked=elp_stacked
         self.elp_yield=elp_yield
         self.dissolve_delay=dissolve_delay
         self.age_bonus=age_bonus
-        self.protocol = protocol
-        self.user_collateral = user_collateral
+        self.spend_icp=0
+        self.spend_dollars = 0
+        self.reward_from_protocol = 0
     @property
     def voting_power(self):
         return (self.elp_stacked+self.elp_yield)*self.dissolve_delay*self.age_bonus
 
     def provide_liquidity(self,amount:Icp):
-        self.protocol.current_collateral += amount
-        self.protocol.fee_available+=self.protocol.percentage_fee*amount.to_eusd+self.protocol.base_fee
+        if amount>0:
+            self.protocol.current_collateral += amount
+            if self not in self.protocol.supporters:
+                self.protocol.supporters.add(self)
 
        
 
 
 class StableSeeker:
-    def __init__(self,protocol:Protocol,initial_price:float,user_collateral:Icp):
+    def __init__(self,protocol:Protocol,icp:Icp=Icp(0)):
         self.protocol = protocol
-        self.initial_price = initial_price
-        self.user_collateral = user_collateral
-        self.user_collateral_eusd = self.user_collateral.to_eusd
-        self.protocol.current_collateral+=self.user_collateral # deposit
-        self.protocol.fee_available+=self.protocol.percentage_fee*self.user_collateral_eusd+self.protocol.base_fee #fee on deposit
-        self.protocol.mint(self.user_collateral_eusd)  
-    
-    def withdraw(self,amount:float):
-        if amount<=self.user_collateral_eusd:
-            self.user_collateral_eusd-=amount
+        self.icp_spend = 0
+        self.current_eusd = 0
+        self.current_icp = icp
+        self.dollars_spend = icp.to_eusd
+
+    def add_icp_to_wallet(self,icp:Icp):
+        self.current_icp+=icp
+        self.icp_spend+=icp
+        self.dollars_spend+= icp.to_eusd
+
+    def depose_icp_to_protocol(self,amount:Icp):
+        if amount>self.current_icp:
+            self.add_icp_to_wallet(amount-self.current_icp)
+        self.current_icp-= amount
+        self.protocol.current_collateral+=amount
+        fee = self.protocol.calculate_fee(amount)
+        self.protocol.fee_available+=fee 
+        self.current_eusd += amount.to_eusd-fee.to_eusd
+        self.protocol.mint(self.current_eusd) 
+
+        
+    def withdraw_icp_from_protocol(self,amount:float):
+        
+        if amount<=self.current_eusd:
+            self.current_eusd-=amount
             self.protocol.current_collateral-=Icp(amount/Icp.current_value)
-            self.protocol.fee_available+=self.protocol.percentage_fee*amount+self.protocol.base_fee
-            self.protocol.burnt(amount)  
+            fee = self.protocol.calculate_fee(amount)
+            self.protocol.fee_available+=fee
+            self.current_icp+=Icp((amount-fee)/Icp.current_value)  
+            self.protocol.burnt(amount)
         else:
             raise ValueError('Cannot whithdraw more than what you have in your wallet')
 
--- test/test.py
+++ test/test.py
@@ -1,7 +1,7 @@
 from model import StableSeeker,Protocol,Icp
 import unittest
 class ElliptictTest(unittest.TestCase):
-    protocol = Protocol(Icp(30),100,base_fee=2)
+    protocol = Protocol(Icp(30),100,base_fee=0)
     def test_collateral_ratio(self):
         assert(self.protocol.is_over_collateralized)
     def test_mint_burnt(self):
@@ -14,14 +14,16 @@ class ElliptictTest(unittest.TestCase):
         assert(self.protocol.current_collateral_value==60)
 
     def test_add_stable_protocol(self): 
-        stable1 = StableSeeker(self.protocol,Icp.current_value,Icp(20))
-        stable2 = StableSeeker(self.protocol,Icp.current_value,Icp(20))
+        stable1 = StableSeeker(self.protocol,Icp(20))
+        stable2 = StableSeeker(self.protocol,Icp(20))
+        stable1.depose_icp_to_protocol(Icp(20))
+        stable2.depose_icp_to_protocol(Icp(20))
         assert(self.protocol.current_collateral==70)
-        assert(stable1.user_collateral_eusd==100)
+        assert(stable1.current_eusd==100)
         Icp.current_value = 10
-        assert(stable1.ratio==2)
-        stable1.withdraw(stable1.user_collateral_eusd)
-        stable2.withdraw(stable2.user_collateral_eusd)
+        assert(stable1.dollars_spend/(stable1.current_eusd+stable1.current_icp*Icp.current_value)==2)
+        stable1.withdraw_icp_from_protocol(stable1.user_collateral_eusd)
+        stable2.withdraw_icp_from_protocol(stable2.user_collateral_eusd)
         assert(self.protocol.current_collateral==50)
         print(self.protocol.collateral_ratio)
         
